1155b61ea39b2fd52d36c5cf951486b0c8db67a5
MusthafaAltaf2001 <musthafaaltaf2001@gmail.com>
2024-11-22 02:36:58 +0530

    refactor: removed unnecessary code in route

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,11 +1,6 @@
 import { NextResponse } from 'next/server'
 import profiles from '../data.json'
 
-type ResponseData = {
-    message: string,
-    data: Profile[]
-}
-
 export async function GET() {
     return NextResponse
         .json({
